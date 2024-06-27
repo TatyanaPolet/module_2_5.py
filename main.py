b506f7cd000a7d873d04e8b0b3fba78469a2330b
TatyanaPolet <tatyana_polet@mail.ru>
2024-06-27 12:18:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+def get_matrix (n, m, value):
+    matrix = []
+    for i in range (n):
+        matr = []
+        matrix.append(matr)
+        for j in range(m):
+            matr.append(value)
+    return matrix
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
+
+
+
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
